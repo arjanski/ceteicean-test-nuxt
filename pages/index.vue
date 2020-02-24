eed126372c506a0d5d4f3fcb9017eec4f2672548
Arjan Dhupia <dhupia@bbaw.de>
2020-02-24 15:51:28 +0100

    Initial app setup, CETEIcean error ("window not defined")

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="height: 3000px" v-html="TEI"></div>
+</template>
+
+<script>
+import CETEI from '../CETEIcean/CETEI.js'
+export default {
+  name: 'TEITest',
+  data() {
+    return {
+      TEI: null
+    }
+  },
+  created() {
+    var CETEIcean = new CETEI()
+    var self = this
+    CETEIcean.getHTML5(
+      'https://gregorovius-edition.dhi-roma.it/api/letters/G000003',
+      function(data) {
+        console.log(data.outerHTML)
+        self.TEI = data.outerHTML
+      }
+    )
+  }
+}
+</script>
